6e9a52c11af3b0828a3c1bb47740acd20540253b
Lucas Araújo <lucaraj1412@gmail.com>
2025-03-23 19:34:17 -0300

    feat: add ex 3 and 4 week

---
+++ Exercício4.R
@@ -0,0 +1,7 @@
+# 1 - Um fator também pode ser gerado usando a função gl(). Qual o comando cria um fator com 100 repetições tanto para"M" quanto para "F" ? Note que o comando deve ser submetido no seguinte formato: gl(x,k,labels = z). OBS.: Ao submeter o comando, retire todos os espaço em branco.
+
+# gl(2,100,labels=c("M","F"))
+
+# 2 - Crie o fator drinks <- factor(c("beer","beer","wine","water")). Depois, submeta apenas o comando em R para calcular a proporção de cervejas (beer) no objeto fator de bebidas (drinks). É obrigatório o uso da função mean para calcular a proporção.  OBS.: Ao submeter o comando, retire todos os espaço em branco.
+
+drinks <- factor(c("beer","beer","wine","water"))
